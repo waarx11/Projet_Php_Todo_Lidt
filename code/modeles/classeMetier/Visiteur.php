3a8641e97bca6f300c7cafec9e8701ec8794ec6f
nathan <nathan@dbNathan>
2022-12-03 00:26:11 +0100

    Finalisation des classe métiers

--- code/modeles/classeMetier/Visiteur.php
+++ code/modeles/classeMetier/Visiteur.php
@@ -3,5 +3,39 @@
 namespace classeMetier;
 class Visiteur
 {
+    private string $id;
+    private string $nom;
+    private string $mdp;
 
+    public function __construct(string $id, string $nom, string $mdp){
+        $this->id=$id;
+        $this->nom=$nom;
+        $this->mdp=$mdp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMdp()
+    {
+        return $this->mdp;
+    }
+
+    
 }
