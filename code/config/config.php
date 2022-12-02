b270ef5fcc8416b4319d60eb6b7e0b2340354933
rakhedair <rami.khedair@etu.uca.fr>
2022-12-02 19:26:52 +0100

    changement de structure de projet

---
+++ code/config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+//gen
+$rep= __DIR__ . '/mvc2_autoloadsimple/';
+
+// liste des modules à inclure
+
+//$dConfig['includes']= array('controleur/Validation.php');
+
+
+
+//BD
+
+$base="";
+$login="";
+$mdp="";
+
+//Vues
+
+$vues['erreur']='vues/erreur.php';
+$vues['vuephp1']='vues/vuephp1.php';
+
+
+?>
